e0752a5c57b9c0d2b18d9a4db3cfe390617e80f3
Tal Zion <talezion@gmail.com>
2016-07-27 19:33:51 +0100

    Setting example project

---
+++ COS_Example/COS_Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  COS_Example
+//
+//  Created by Tal Zion on 27/07/2016.
+//  Copyright © 2016 Tal Zion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
